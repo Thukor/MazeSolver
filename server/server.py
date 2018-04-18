c1392a8414b3c0597a017248e12515bbf96699ef
Thukor <encarc@rpi.edu>
2018-04-18 16:57:59 -0400

    .

--- server/server.py
+++ server/server.py
@@ -27,7 +27,7 @@ class MazeSolverRequestHandler(BaseHTTPRequestHandler):
 	def _solve_maze(self):
 		content_length = int(self.headers['Content-Length'])
 		image = self.rfile.read(content_length)
-		print(content_length)
+		print(image)
 		with open("maze.jpg", 'wb') as ms:
 			ms.write(image)
 
