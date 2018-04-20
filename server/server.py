e9772cd1a6a76b4bde8d3b2623bfb0e7ae3b2f3a
Christian Encarnacion <encarc@rpi.edu>
2018-04-19 21:01:40 -0400

    Update server.py

--- server/server.py
+++ server/server.py
@@ -6,12 +6,17 @@ from MazeJsonManager import *
 from ImageProcessor import *
 import base64
 
+
+"""
+Maze Solver Request Handler (for server)
+"""
 class MazeSolverRequestHandler(BaseHTTPRequestHandler):
 
 	def _set_headers_for_simple_acknowledgement(self):
 		self.send_response(200)
 		self.end_headers()
 
+	#simple ping to server
 	def _set_hello_header(self):
 		self.send_response(200)
 		self.send_header("Content-type", "text")
@@ -23,15 +28,20 @@ class MazeSolverRequestHandler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("Content-type", "image/jpg")
 		self.end_headers()
+		
+		#encode bytes for jpg
 		img_bytes = base64.b64encode(image.read())
 		image.close()
 		data = open('dump.txt', 'wb')
 		data.write(img_bytes)
 		data.close()
+		
 		self.wfile.write(img_bytes)
 
 	def _solve_maze(self):
 		content_length = int(self.headers['Content-Length'])
+		
+		#decode bytes from jpg received from application
 		image = base64.b64decode(self.rfile.read(content_length))
 
 		with open("maze.jpg", 'wb') as ms:
@@ -41,29 +51,17 @@ class MazeSolverRequestHandler(BaseHTTPRequestHandler):
 		self._set_headers_for_send_image("solution.jpg")
 
 	def do_GET(self):
-		print("GET" + str(self.path))
 		if "/solutions" in self.path:
 			info = self.path[1:].split("/")
 			maze_image = info[1]
 			self._set_headers(maze_image)
 		if "/hello" in self.path:
-			print("GOT A REQUEST")
 			self._set_hello_header()
 			
 	def do_POST(self):
-		print(self.path)
 		if "/solve" in self.path:
-			print("WE GOT AN IMAGE GUYS!!!!!")
 			self._solve_maze()
-		# if "/demo" in self.path:
-		# 	content_length = int(self.headers['Content-Length'])
-		# 	image = self.rfile.read(content_length)
-		# 	table = MazeJsonManager.load_cell_table("table.json")
-		# 	pprint("GOT TABLE\n")
-		# 	pprint(table)
-		# 	self._set_headers_for_send_image("maze_output.png")			
-
-
+		
 if __name__ == "__main__":
 	server_class = HTTPServer
 	httpd = server_class(("", 80), MazeSolverRequestHandler)
