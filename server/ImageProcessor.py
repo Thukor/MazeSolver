50ee68fbdf4c7dc27ce159eeb8742b3a78eb5768
Christian Encarnacion <encarc@rpi.edu>
2018-04-19 20:59:34 -0400

    Update ImageProcessor.py

--- server/ImageProcessor.py
+++ server/ImageProcessor.py
@@ -1,10 +1,17 @@
 from image_processing import *
 
+"""
+Processor Class for images
+"""
+
+
 class ImageProcessor:
 
+	#initialize strategies
 	def __init__(self,strategies):
 		self.strategies = [birdseye_correction, image_segmentation]
-
+	
+	#We interpret each set of processing functions as strategies.
 	def process_image(image_name, number):
 		birdseye_correction(image_name, number)
 		image_segmentation("warped.png", number)
